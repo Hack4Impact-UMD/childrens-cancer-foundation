afd3071cf55c0af12d7733e3f001f935ea6c0617
Arnav Dadarya <37542991+ardada2468@users.noreply.github.com>
2025-04-03 13:00:09 -0400

    update the firebase functons

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -2,59 +2,57 @@
 /**
  * Import function triggers from their respective submodules:
  *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ * import { onCall, onRequest } from "firebase-functions/v2/https";
  *
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.addAdminRole = exports.addApplicantRole = exports.addReviewerRole = void 0;
-const functions = require("firebase-functions");
+const { onRequest, onCall } = require("firebase-functions/v2/https");
 const admin = require("firebase-admin");
 admin.initializeApp();
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
+
+// HTTP function using V2 onRequest trigger
+exports.helloWorld = onRequest((request, response) => {
+  response.send("Hello from Firebase!");
 });
-exports.addReviewerRole = functions.https.onCall((data, context) => {
-    return admin.auth().getUserByEmail(data.email).then((user) => {
-        return admin.auth().setCustomUserClaims(user.uid, {
-            "role": "reviewer"
-        });
-    }).then(() => {
-        return {
-            message: `Success! ${data.email} has been made a reviewer.`
-        };
-    }).catch((err) => {
-        return err;
-    });
-});
-exports.addApplicantRole = functions.https.onCall((data, context) => {
-    return admin.auth().getUserByEmail(data.email).then((user) => {
-        return admin.auth().setCustomUserClaims(user.uid, {
-            "role": "applicant"
-        });
-    }).then(() => {
-        return {
-            message: `Success! ${data.email} has been made an applicant.`
-        };
-    }).catch((err) => {
-        return err;
-    });
-});
-exports.addAdminRole = functions.https.onCall((data, context) => {
-    return admin.auth().getUserByEmail(data.email).then((user) => {
-        return admin.auth().setCustomUserClaims(user.uid, {
-            "role": "admin"
-        });
-    }).then(() => {
-        return {
-            message: `Success! ${data.email} has been made an admin.`
-        };
-    }).catch((err) => {
-        return err;
+
+// Callable function using V2 onCall trigger for adding the reviewer role
+exports.addReviewerRole = onCall((data, context) => {
+  return admin.auth().getUserByEmail(data.email)
+    .then((user) => {
+      return admin.auth().setCustomUserClaims(user.uid, { role: "reviewer" });
+    })
+    .then(() => {
+      return { message: `Success! ${data.email} has been made a reviewer.` };
+    })
+    .catch((err) => {
+      return err;
     });
 });
 
-//# sourceMappingURL=index.js.map
+// Callable function using V2 onCall trigger for adding the applicant role
+exports.addApplicantRole = onCall((data, context) => {
+  return admin.auth().getUserByEmail(data.email)
+    .then((user) => {
+      return admin.auth().setCustomUserClaims(user.uid, { role: "applicant" });
+    })
+    .then(() => {
+      return { message: `Success! ${data.email} has been made an applicant.` };
+    })
+    .catch((err) => {
+      return err;
+    });
+});
+
+// Callable function using V2 onCall trigger for adding the admin role
+exports.addAdminRole = onCall((data, context) => {
+  return admin.auth().getUserByEmail(data.email)
+    .then((user) => {
+      return admin.auth().setCustomUserClaims(user.uid, { role: "admin" });
+    })
+    .then(() => {
+      return { message: `Success! ${data.email} has been made an admin.` };
+    })
+    .catch((err) => {
+      return err;
+    });
+});
