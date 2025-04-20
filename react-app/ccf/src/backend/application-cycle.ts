1b0726ce0f40c27b545208f962ccbd2888f64bf3
heavens-potato <sswu2005@gmail.com>
2025-04-19 21:41:48 -0400

    initial admin info page

--- react-app/ccf/src/backend/application-cycle.ts
+++ react-app/ccf/src/backend/application-cycle.ts
@@ -1,20 +1,20 @@
 import {db} from "../index"
 import {collection, query, getDocs, where, limit} from "firebase/firestore"
-import ApplicationCycle from "../types/applicationCycle-types"
+// import ApplicationCycle from "../types/applicationCycle-types"
 
-export const getCurrentCycle = async (): Promise<ApplicationCycle> => {
-    const q = query(collection(db, "applicationCycles"), where("current", "==", true), limit(1))
+// export const getCurrentCycle = async (): Promise<ApplicationCycle> => {
+//     const q = query(collection(db, "applicationCycles"), where("current", "==", true), limit(1))
 
-    const snap = await getDocs(q);
+//     const snap = await getDocs(q);
     
-    return snap.docs[0].data() as ApplicationCycle
-}
+//     return snap.docs[0].data() as ApplicationCycle
+// }
 
-export const getAllCycles = async () : Promise<Array<ApplicationCycle>> => {
-    const q = query(collection(db, "applicationCycles"))
+// export const getAllCycles = async () : Promise<Array<ApplicationCycle>> => {
+//     const q = query(collection(db, "applicationCycles"))
 
-    const snap = await getDocs(q);
-    return snap.docs.map((d) => {
-        return d.data() as ApplicationCycle
-    })
-}
+//     const snap = await getDocs(q);
+//     return snap.docs.map((d) => {
+//         return d.data() as ApplicationCycle
+//     })
+// }
---
+++ react-app/ccf/src/pages/admin-edit-info/AdminEditInformation.tsx
@@ -0,0 +1,243 @@
+import "./AdminEditInformation.css";
+import logo from "../../assets/ccf-logo.png";
+import Sidebar from "../../components/sidebar/Sidebar";
+import { useState, useEffect } from "react";
+import { FaSearch } from "react-icons/fa";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../index";
+import MoreTimeIcon from '@mui/icons-material/MoreTime';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import dayjs, { Dayjs } from 'dayjs';
+import { TextField, Button } from '@mui/material';
+
+function AdminEditInformation(): JSX.Element {
+    const [nextGenDate, setNextGenDate] = useState<Dayjs | null>(dayjs('2025-06-01'));
+    const [researchDate, setResearchDate] = useState<Dayjs | null>(dayjs('2025-06-01'));
+    const [nonResearchDate, setNonResearchDate] = useState<Dayjs | null>(dayjs('2025-06-01'));
+    const [reviewerDate, setReviewerDate] = useState<Dayjs | null>(dayjs('2025-06-01'));
+
+    const handleNextGenChange = (newNextGenDate: Dayjs | null) => {
+        setNextGenDate(newNextGenDate);
+    };
+
+    const handleResearchChange = (newResearchDate: Dayjs | null) => {
+        setResearchDate(newResearchDate);
+    };
+
+    const handleNonResearchChange = (newNonResearchDate: Dayjs | null) => {
+        setNonResearchDate(newNonResearchDate);
+    };
+
+    const handleReviewerDateChange = (newReviewerDate: Dayjs | null) => {
+        setReviewerDate(newReviewerDate);
+    }
+
+    const sidebarItems = [
+        { name: "Home", path: "/" },
+        { name: "Account Settings", path: "/settings" },
+        { name: "All Accounts", path: "/admin-all-accounts" },
+        { name: "Logout", path: "/login" },
+        { name: "Edit Information", path: "/admin-edit-information" }
+    ];
+
+    return (
+        <div>
+            <Sidebar links={sidebarItems} />
+            <div className="dashboard-container">
+                <div className="edit-info-dashboard-header-container">
+                    <img src={logo} className="edit-info-logo" alt="logo" />
+                    <h1 className="edit-info-header">
+                        Edit Information
+                    </h1>
+                </div>
+                <div className="sections-container">
+                    <div className="deadlines-section">
+                        <div className="deadlines-header-container">
+                            <MoreTimeIcon />
+                            <h2 className="deadlines-header-text">Deadlines for Current Cycle</h2>
+                        </div>
+                    </div>
+                    <div className="deadline-interactives">
+                        <h2>Applications:</h2>
+                        <div className="interactive-date-selector">
+                            <h2>NextGen Grant Application</h2>
+                            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                <DatePicker
+                                    value={nextGenDate}
+                                    onChange={handleNextGenChange}
+                                    enableAccessibleFieldDOMStructure={false}
+                                    sx={{
+                                        backgroundColor: '#79747E'
+                                    }}
+                                    slots={{
+                                        textField: (props) => (
+                                            <TextField
+                                                {...props}
+                                                sx={{
+                                                    '& .MuiInputBase-input': {
+                                                        textAlign: 'center',
+                                                        height: '8px',
+                                                        width: '150px',
+                                                        color: '#79747E'
+                                                    },
+                                                    '& .MuiOutlinedInput-notchedOutline': {
+                                                        border: '2px solid #79747E',
+                                                        borderRadius: '15px'
+                                                    },
+                                                }}
+                                            />
+                                        ),
+                                    }}
+                                />
+                            </LocalizationProvider>
+                        </div>
+                        <div className="interactive-date-selector">
+                            <h2>Research Grant Application</h2>
+                            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                <DatePicker
+                                    value={researchDate}
+                                    onChange={handleResearchChange}
+                                    enableAccessibleFieldDOMStructure={false}
+                                    sx={{
+                                        backgroundColor: '#79747E'
+                                    }}
+                                    slots={{
+                                        textField: (props) => (
+                                            <TextField
+                                                {...props}
+                                                sx={{
+                                                    '& .MuiInputBase-input': {
+                                                        textAlign: 'center',
+                                                        height: '8px',
+                                                        width: '150px',
+                                                        color: '#79747E'
+                                                    },
+                                                    '& .MuiOutlinedInput-notchedOutline': {
+                                                        border: '2px solid #79747E',
+                                                        borderRadius: '15px'
+                                                    },
+                                                }}
+                                            />
+                                        ),
+                                    }}
+                                />
+                            </LocalizationProvider>
+                        </div>
+                        <div className="interactive-date-selector">
+                            <h2>Non-Research Grant Application</h2>
+                            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                <DatePicker
+                                    value={nonResearchDate}
+                                    onChange={handleNonResearchChange}
+                                    enableAccessibleFieldDOMStructure={false}
+                                    sx={{
+                                        backgroundColor: '#79747E'
+                                    }}
+                                    slots={{
+                                        textField: (props) => (
+                                            <TextField
+                                                {...props}
+                                                sx={{
+                                                    '& .MuiInputBase-input': {
+                                                        textAlign: 'center',
+                                                        height: '8px',
+                                                        width: '150px',
+                                                        color: '#79747E'
+                                                    },
+                                                    '& .MuiOutlinedInput-notchedOutline': {
+                                                        border: '2px solid #79747E',
+                                                        borderRadius: '15px'
+                                                    },
+                                                }}
+                                            />
+                                        ),
+                                    }}
+                                />
+                            </LocalizationProvider>
+                        </div>
+                        <div className="save-button">
+                            <Button
+                                variant="contained"
+                                sx={{
+                                    backgroundColor: '#79747E',
+                                    fontFamiy: 'Roboto, sans-serif',
+                                    textTransform: 'none',
+                                    height: '40px',
+                                    fontSize: '1.25rem',
+                                    fontWeight: 'normal',
+                                    borderRadius: '10px',
+                                    '&:hover': {
+                                        backgroundColor: '#003E83'
+                                    },
+                                    marginBottom: '10px',
+                                    marginTop: '10px'
+                                }}
+
+                            >Set Application Deadline</Button>
+                        </div>
+                    </div>
+                    <div className="deadline-interactives">
+                        <h2>Reviews:</h2>
+                        <div className="interactive-date-selector">
+                            <h2>Reviewer Responses</h2>
+                            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                <DatePicker
+                                    value={reviewerDate}
+                                    onChange={handleReviewerDateChange}
+                                    enableAccessibleFieldDOMStructure={false}
+                                    sx={{
+                                        backgroundColor: '#79747E',
+                                    }}
+                                    slots={{
+                                        textField: (props) => (
+                                            <TextField
+                                                {...props}
+                                                sx={{
+                                                    '& .MuiInputBase-input': {
+                                                        textAlign: 'center',
+                                                        height: '8px',
+                                                        width: '150px',
+                                                        color: '#79747E',
+                                                    },
+                                                    '& .MuiOutlinedInput-notchedOutline': {
+                                                        border: '2px solid #79747E',
+                                                        borderRadius: '15px'
+                                                    },
+                                                }}
+                                            />
+                                        ),
+                                    }}
+                                    
+                                />
+                            </LocalizationProvider>
+                        </div>
+                        <div className="save-button">
+                            <Button
+                                variant="contained"
+                                sx={{
+                                    backgroundColor: '#79747E',
+                                    fontFamiy: 'Roboto, sans-serif',
+                                    textTransform: 'none',
+                                    height: '40px',
+                                    fontSize: '1.25rem',
+                                    fontWeight: 'normal',
+                                    borderRadius: '10px',
+                                    '&:hover': {
+                                        backgroundColor: '#003E83'
+                                    },
+                                    marginBottom: '10px',
+                                    marginTop: '10px'
+                                }}
+
+                            >Set Reviewer Deadline</Button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AdminEditInformation;
