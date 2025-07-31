8c24a37ae934090b915e552bce80b61639b1caef
Javid Fathi <javidfathi@microsoft.com>
2025-07-30 18:36:47 -0700

    Markdown init draft

---
+++ react-app/ccf/src/components/markdown/Markdown.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import { TextField, Box } from '@mui/material';
+import Markdown from 'markdown-to-jsx';
+
+export default function MarkdownPreviewer(_previewOnly: boolean = false): JSX.Element {
+    const [previewOnly, setPreviewOnly] = useState(_previewOnly);
+    const [text, setText] = useState('');
+    const handleTextChange = (e: any) => {
+        setText(e.target.value);
+    };
+    return (
+        <Box className="markdown">
+            {(!previewOnly) ? (
+                <TextField
+                label="Enter Markdown"
+                multiline
+                minRows={6}
+                fullWidth
+                onChange={handleTextChange}
+                variant="outlined"
+                className="markdown-input"/>
+            ) : (null)}
+            <Box className="markdown-previewer" sx={{ p: 2, border: '1px solid #ccc', borderRadius: 2, minHeight: 200, background: '#fafafa' }}>
+                <Markdown>{text}</Markdown>
+            </Box>
+        </Box>
+    );
+}
