67004ae82e19608d71bdab3ed9e0b4788cd9bb7d
Javid Fathi <javidfathi@microsoft.com>
2025-08-16 20:55:34 -0700

    WIP editable - updating expanded logic

--- react-app/ccf/src/components/faq/FaqEditableComp.tsx
+++ react-app/ccf/src/components/faq/FaqEditableComp.tsx
@@ -9,23 +9,23 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 
 const EditableFAQComponent: React.FC<FAQComponentProps> = ({ faqs }) => {
-    const [activeIndex, setActiveIndex] = useState<number | null>(null);
     const [editingIndex, setEditingIndex] = useState<number | null>(null);
-    const [questions, setQuestions] = useState<{[key: number]: string}>(() => {
-        const initialQuestions: {[key: number]: string} = {};
+    const [questions, setQuestions] = useState<{[key: number]: {question: string, expanded: boolean}}> (() => {
+        const initialQuestions: {[key: number]: {question: string, expanded: boolean}} = {};
         faqs.forEach((faq, index) => {
-            initialQuestions[index] = faq.question;
+            initialQuestions[index] = { question: faq.question, expanded: false };
         });
         return initialQuestions;
     });
 
     const toggleFAQ = (index: number) => {
-        setActiveIndex(activeIndex === index ? null : index);
+        questions[index].expanded = !questions[index].expanded;
     };
 
     const toggleEdit = (index: number, event: React.MouseEvent) => {
         event.stopPropagation();
         setEditingIndex(editingIndex === index ? null : index);
+        questions[index].expanded = true;
     };
 
     const updateFAQ = (id: string, question: string, answer: string) => {
@@ -38,12 +38,11 @@ const EditableFAQComponent: React.FC<FAQComponentProps> = ({ faqs }) => {
                 <div key={index} className="faq-item">
                     <div
                         className="faq-question"
-                        onClick={() => toggleFAQ(index)}
                     >
                         <div className="faq-question-header">
-                            <div className="expand-icon">{activeIndex === index ? <ExpandLessIcon /> : <ExpandMoreIcon />}</div>
+                            <div className="expand-icon" onClick={() => toggleFAQ(index)}>{questions[index].expanded ? <ExpandLessIcon /> : <ExpandMoreIcon />}</div>
                             <Button
-                                onClick={(e) => toggleEdit(index, e)}
+                                onClick={(e) => { toggleEdit(index, e); }}
                                 className="edit-toggle-button"
                                 startIcon={<EditIcon />}
                                 size="small"
@@ -55,20 +54,20 @@ const EditableFAQComponent: React.FC<FAQComponentProps> = ({ faqs }) => {
                                 <Box>
                                     <TextField
                                         label="Update Frequently Asked Question"
-                                        value={questions[index] || faq.question}
-                                        onChange={(e) => setQuestions({ ...questions, [index]: e.target.value })}
+                                        value={questions[index].question}
+                                        onChange={(e) => setQuestions({ ...questions, [index]: { ...questions[index], question: e.target.value } })}
                                         variant="outlined"
                                         sx={{ minWidth: '100%' }}
                                         className="markdown-input" />
                                 </Box>
                             ) : (
-                                <Typography variant="h6" component="p" style={{ marginLeft: '8px' }}>
-                                    {questions[index] || faq.question}
+                                <Typography variant="h6" onClick={() => toggleFAQ(index)} component="p" style={{ marginLeft: '8px' }}>
+                                    {questions[index].question || faq.question}
                                 </Typography>
                             )}
                         </div>
                     </div>
-                    {activeIndex === index && (
+                    {questions[index].expanded && (
                         <div className="faq-answer">
                             <div className="markdown-preview-light">
                                 <MarkdownPreviewer _text={faq.answer} _previewOnly={editingIndex !== index} />
