8e1e3cbae68f3b74cb76ed4bee9b2d83f11cc0c0
Pooja <praghura@terpmail.umd.edu>
2026-04-14 17:50:47 -0400

    fixed issue with blue connector line being of wrong length for non research

--- react-app/ccf/src/pages/application-form/Components/Breadcrumbs.tsx
+++ react-app/ccf/src/pages/application-form/Components/Breadcrumbs.tsx
@@ -7,9 +7,14 @@ interface BreadcrumbProps {
 }
 
 const Breadcrumb: React.FC<BreadcrumbProps> = ({ currentPage, pages }) => {
+  const connectorInset = `${50 / pages.length}%`;
+
   return (
     <div className="breadcrumbs-wrapper">
-      <div className="breadcrumbs">
+      <div
+        className="breadcrumbs"
+        style={{ ["--breadcrumb-connector-inset" as any]: connectorInset }}
+      >
     {pages.map((currPage, key) => (
         <div className="breadcrumb-container" key={key}>
             <button className={currentPage === key + 1 ? 'breadcrumb-circle-active' : 'breadcrumb-circle'} />
