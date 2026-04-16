7167ccf0346618d72f7babd31695bd53e44f779a
Manan Zaveri <mananzaveri27@gmail.com>
2026-04-16 13:30:22 -0400

    matched ui across login page, create account page, and forgot password page

--- react-app/ccf/src/pages/create-acc-menu/CreateAccMenu.tsx
+++ react-app/ccf/src/pages/create-acc-menu/CreateAccMenu.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./CreateAccMenu.css";
+import "../login/login.css";
+import "./CreateAccMenu.css"
 import Button from "../../components/buttons/Button";
 import { useState } from "react";
 import DrHanleyLabImage from "../../assets/Dr. Hanley Lab 1.png";
@@ -24,42 +25,40 @@ function CreateAccMenu() {
         };
       }, []);
   return (
-    <div className="createAccMenu-container">
-      <div className="createAccMenu-form">
-        <div className="createAccMenu-logo">
-          <img src="/ccflogo.png" alt="Logo" className="createAccMenu-logoImage" />
-        </div>
-        <h1 className="createAccMenu-heading">Create Account</h1>
-        <p>Which account are you creating?</p>
-        <Link to='/create-account-applicants' className="createAccLink">
-        <Button 
-            variant="red" 
-            width="95%" 
-            height="50px" 
-            borderRadius="25px">
-          Applicant
-        </Button>
-        </Link>
-        <Link to='/create-account-reviewers' className="createAccLink">
-        <Button 
-            variant="red"
-            width="95%" 
-            height="50px" 
-            borderRadius="25px">
-          Reviewer
-        </Button>
-        </Link>
-        <Link to="/login" className="backToLogin"><u>Back to log in</u></Link>
-      </div>
-      {isWideScreen && (
-          <div className="imageContainer">
-            <img src={DrHanleyLabImage} alt="Dr Hanley in the lab" className="createAcc-image" />
-            <img src={toretsky} alt="Researcher with patient" className="createAcc-image" />
-            <div className="createAcc-yellowOverlay">
-              <img src={yellowOverlay} alt="" aria-hidden="true" className="createAcc-yellow" />
-            </div>
+    <div className="login-container">
+      <div className="login-content">
+        <div className="login-form">
+          <div className="logo">
+            <img src="/ccflogo.png" alt="Logo" className="logoImage" />
           </div>
-        )}
+          <h1 className="global-header">Create Account</h1>
+          <p>Which account are you creating?</p>
+          <Link to='/create-account-applicants' className="createAccLink">
+          <Button 
+              variant="red" 
+              className="login-button">
+            Applicant
+          </Button>
+          </Link>
+          <Link to='/create-account-reviewers' className="createAccLink">
+          <Button 
+              variant="red"
+              className="login-button">
+            Reviewer
+          </Button>
+          </Link>
+          <Link to="/login" className="backToLogin"><u>Back to log in</u></Link>
+        </div>
+        {isWideScreen && (
+            <div className="login-imageContainer">
+              <img src={DrHanleyLabImage} alt="Dr Hanley in the lab" className="login-image" />
+              <img src={toretsky} alt="Researcher with patient" className="login-image" />
+              <div className="login-yellowOverlay">
+                <img src={yellowOverlay} alt="" aria-hidden="true" className="login-yellow" />
+              </div>
+            </div>
+          )}
+        </div>
     </div>
   );
 }
