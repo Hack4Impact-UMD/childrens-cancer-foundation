8fbff38eb926494c7f4976d348fff6100c3ccec9
Chase Fournier <fournier.chase2@gmail.com>
2026-08-26 17:19:35 -0400

    Contact Information

--- react-app/ccf/src/App.tsx
+++ react-app/ccf/src/App.tsx
@@ -39,6 +39,7 @@ import AssignReviewers from "./pages/assign-reviewers-page/AssignReviewers";
 import AdminWhitelistReviewers from "./pages/admin-whitelist/AdminWhitelistReviewers";
 import AdminPostGrantReports from "./pages/admin-post-grant-reports/AdminPostGrantReports";
 import GrantInstructions from "./pages/grant-instructions/GrantInstructions";
+import { CCF_CONTACT_INFO } from "./StaticData/CONTACT-INFO";
 
 // import AssignReviewersPage from "./pages/assign-reviewers-page/AssignReviewers";
 
@@ -75,7 +76,7 @@ function App(): JSX.Element {
           }
         />
         <Route path="/reviewer/dashboard" element={
-          <ReviewerProtectedRoute element={<ReviewerDashboard email={"email@testing.org"} hours={"10am - 5pm weekdays"} phone={"111-222-3333"}></ReviewerDashboard>} />
+          <ReviewerProtectedRoute element={<ReviewerDashboard {...CCF_CONTACT_INFO}></ReviewerDashboard>} />
         }>
         </Route>
         <Route
@@ -214,7 +215,7 @@ function App(): JSX.Element {
         />
 
         <Route path="/reviewer-dashboard" element={
-          <ReviewerProtectedRoute element={<ReviewerDashboard email={"email@testing.org"} hours={"10am - 5pm weekdays"} phone={"111-222-3333"}></ReviewerDashboard>} />
+          <ReviewerProtectedRoute element={<ReviewerDashboard {...CCF_CONTACT_INFO}></ReviewerDashboard>} />
         }>
         </Route>
         <Route
--- react-app/ccf/src/pages/applicant-dashboard/ApplicantDashboard.tsx
+++ react-app/ccf/src/pages/applicant-dashboard/ApplicantDashboard.tsx
@@ -4,6 +4,7 @@ import { FaFileAlt, FaArrowRight, FaClipboardList, FaQuestionCircle, FaEnvelope 
 import Button from "../../components/buttons/Button";
 import FAQComponent from "../../components/faq/FaqComp";
 import ContactUs from "../../components/contact/ContactUs";
+import { CCF_CONTACT_INFO } from "../../StaticData/CONTACT-INFO";
 import Banner from "../../components/banner/Banner";
 import DashboardSection from "../../components/dashboard-layout/DashboardSection";
 import RoleDashboardShell from "../../components/dashboard-layout/RoleDashboardShell";
@@ -502,11 +503,7 @@ function ApplicantUsersDashboard(): JSX.Element {
               icon={<FaEnvelope className="dashboard-section-icon" />}
               defaultCollapsed
             >
-              <ContactUs
-                email={"contact@ccf.org"}
-                phone={"111-222-3333"}
-                hours={"Monday - Friday 10AM - 5PM"}
-              />
+              <ContactUs {...CCF_CONTACT_INFO} />
             </DashboardSection>
           </div>
     </RoleDashboardShell>
