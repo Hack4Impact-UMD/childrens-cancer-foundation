a398b378c1e24da32185448fdbeb6dd9834e2a08
Chase Fournier <fournier.chase2@gmail.com>
2026-08-26 17:19:35 -0400

    Contact Information

--- react-app/ccf/src/components/contact/ContactUs.tsx
+++ react-app/ccf/src/components/contact/ContactUs.tsx
@@ -11,7 +11,7 @@ const ContactUs: React.FC<ContactUsProps> = ({ email, phone, hours }) => {
             <div className="contact-method">
                 <div className="contact-method-header">
                     <img src={Email} alt="Email Icon" className="contact-icon" />
-                    <h2 className="contact-method-title">Email Support</h2>
+                    <h2 className="contact-method-title">Contact CCF</h2>
                 </div>
                 <p className="contact-text">Email us and we'll get back to you as soon as possible.</p>
                 <a href={`mailto:${email}`} className="contact-link">
@@ -22,7 +22,7 @@ const ContactUs: React.FC<ContactUsProps> = ({ email, phone, hours }) => {
             <div className="contact-method">
                 <div className="contact-method-header">
                     <img src={Phone} alt="Phone Icon" className="contact-icon" />
-                    <h2 className="contact-method-title">Call Support</h2>
+                    <h2 className="contact-method-title">Call CCF</h2>
                 </div>
                 <p className="contact-text">Call us and we'll get back to you as soon as possible.</p>
                 <a href={`tel:${phone}`} className="contact-link">
